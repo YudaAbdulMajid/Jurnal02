5dca293088ee072785b86cbf300df1bbfee815c8
YudaAbdulMajid <adiyuda@student.telkomuniversity.ac.id>
2021-09-30 19:30:03 +0700

    PerkenalanYuda.java

---
+++ PerkenalanYuda.java
@@ -0,0 +1,8 @@
+public class PerkenalanYuda {
+	
+	public static void main(String[] args) {
+			System.out.println("NAMA : Yuda Abdul Majid");
+			System.out.println("NIM : 6706210129");
+			System.out.println("Kelas : D3RPLA4505");
+	}
+}
